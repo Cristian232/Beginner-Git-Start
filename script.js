9f424ba4f13262261324de51ba9a8ccbf2c6e440
Cristian <Cristi@noemail.com>
2023-02-09 03:15:39 +0200

    Testing purposes not checked commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function myFunction(p1, p2) {
+    return p1 * p2;
+  }
+
+switch(formatGeo){
+
+    case "trapez" :
+        "Variabila mea e x"
+    break;
+
+    case "triunghio" :
+        "Variabila mea e o"
+    break;
+
+
+
+    default: 
+    "nu e o forma "
+}
+
+
+
+
+
+
+
+
